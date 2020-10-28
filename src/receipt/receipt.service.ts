22f73b4151145d08dc83638ba8abc0a601ea23bf
rafanel2 <38199191+rafanel2@users.noreply.github.com>
2020-10-28 14:46:08 -0300

    Error handler

--- src/receipt/receipt.service.ts
+++ src/receipt/receipt.service.ts
@@ -51,7 +51,7 @@ class ReceiptService {
             } catch (error) {
                 if (error?.name === 'TimeoutError') throw new HttpException('Erro tempo limite excedido', HttpStatus.REQUEST_TIMEOUT);
                 this.logger.error((typeof error == 'object' ? JSON.stringify(error.message) : error.message));
-                throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
+                throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
             throw new HttpException('Wrong code provided', HttpStatus.BAD_REQUEST)
